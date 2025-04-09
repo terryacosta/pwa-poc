ebf17a3a0cb38b3b350db70f23d67434985c2ac7
Terry Acosta <terry_acosta@yahoo.com>
2025-04-09 21:07:53 +0000

    initial commit

---
+++ sw.js
@@ -0,0 +1,22 @@
+<!-- sw.js -->
+<script type="text/javascript">
+  self.addEventListener('install', function (e) {
+    e.waitUntil(
+      caches.open('stock-pwa').then(function (cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/manifest.json'
+        ]);
+      })
+    );
+  });
+
+  self.addEventListener('fetch', function (e) {
+    e.respondWith(
+      caches.match(e.request).then(function (response) {
+        return response || fetch(e.request);
+      })
+    );
+  });
+</script>
